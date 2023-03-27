49db6acfb9b388a9d12940612ef8a762b0bf2085
Maxence <maxencedubois22@gmail.com>
2023-03-27 11:13:22 +0200

    max

--- clientVueJS/src/components/ProfilComponents/MesCommandesProfil.vue
+++ clientVueJS/src/components/ProfilComponents/MesCommandesProfil.vue
@@ -1,6 +1,32 @@
 <script setup></script>
 <template>
-    <p> commande</p>
+    <div class="info-container-title">
+        <img src="../../assets/IconProfil/angle-petit-droit.png" alt="">
+        <h2 class="info-title">
+            Mes Commandes
+        </h2>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+    font-family: 'Space-Grotesk-Bold';
+    margin: 0;
+    padding: 0;
+}
+
+.info-container-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 60%;
+}
+
+.info-title {
+    padding: 20px;
+    font-size: 28px;
+    border-bottom: solid 2px;
+
+}
+</style>
