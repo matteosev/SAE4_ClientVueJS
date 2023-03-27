615032ed4f6334b8ebc9c2490522292c4715b77f
Tyrmaar <timote.policet@gmail.com>
2023-03-27 16:19:32 +0200

    produits

--- clientVueJS/src/components/productVue.vue
+++ clientVueJS/src/components/productVue.vue
@@ -1,209 +1,41 @@
-    <!-- component CREE POUR LES BEST SELLERS, J'ai pas encore fait le carousel 
-    et je fais l'affichage d'un Json vu que j'ai pas la base (CSS non définitif, oui il est degueu) -->
-
-
-    <script setup>
-    import axios from 'axios';
-    </script>
-
 <script>
 export default {
-  data() {
-    return {
-      categories: [],
-      categoriesParent:[],
-      categoriesEnfant:[],
-    };
-  },
-  mounted() {
-    this.getAllCategories();
-  },
-  computed:{
-    CategoriesParent(){
-      let categoriesParent = [];
-      for(let categorie of this.categories){
-        if(categorie.categorieParentId==null){
-          categoriesParent.push(categorie);
-        }
-      }
-      return categoriesParent;
+    props: ['id', 'libelle'],
+    data() 
+    {
+        return {
+        };
     }
-  },
-  methods: {
-    getAllCategories() {
-      axios.get('https://localhost:7259/api/Categories/GetAll')
-      .then(response => {
-        console.log(response.data)
-        this.categories = response.data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    },
-    getCategoriesEnfant(categorieParent){
-      let categoriesEnfant = [];
-      for(let categorie of this.categories){
-        if(categorie.categorieParentId == categorieParent.categorieId){
-          categoriesEnfant.push(categorie);
-        }
-      }
-      return categoriesEnfant;
-    }
-  },
-};
+}
 </script>
 
-
-
-
 <template>
-    <nav>
-      <ul>
-        <li class="deroulant" v-for="catParent in CategoriesParent" :key="catParent.categorieId">
-          <a>{{ catParent.libelle }}</a>
-          <ul class="sous">
-            <li v-for="catEnfant in getCategoriesEnfant(catParent)" :key="catEnfant.categorieId">
-              <a>{{ catEnfant.libelle }}</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </template>
+    <div class="Bien">
+      <div>
+        <p>{{ libelle }}</p>
+      </div>
+      <div>
+        <p>{{ libelle }}</p>
+      </div>
+    </div>
+</template>
 
+<style>
+  .Bien{
+          background-color: #dddddd;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          width: 25vw;
+          height: 30vh;
+  }
 
-    <style scoped>
-    body{
-        height: 100vh;
-    }
-    img{
-        height: 10%;
-    }
-    nav{
-        width: 100%;
-        background-color: #fff;
-        top: 0px;
-    }
+  .Bien:hover{
+    cursor: pointer;
+  }
 
-    nav ul{
-        width: 92vw;
-        display: flex;
-        justify-content: space-around;
-        
-        
-    }
+  
 
-    nav ul li{
-        display: flex;
-        width:fit-content;
-        text-align: center;
-        align-items: center;
-        position: relative;
-        margin: 12px;
-    }
+  
 
-    nav ul::after{
-        content:"";
-        clear: both;
-    }
-
-    nav a{
-        text-decoration: none;
-        color: black;
-        border-bottom: 2px solid transparent;
-    }
-
-    nav a:hover{
-        color: var(--first-color);
-        border-bottom: 2px solid rgb(1, 46, 0);
-    }
-
-    .sous{
-        display: none;
-        box-shadow: 0px 1px 2px #CCC;
-        background-color: #fff;
-        position: absolute;
-        width: 200px;
-        height: fit-content;
-        top:100%;
-        z-index: 1000;
-    }
-
-    nav > ul li:hover .sous{
-        display: block;
-    }
-
-    .sous li{
-        float: none;
-        width: fit-content;
-        text-align:left;
-        cursor: pointer;
-    }
-
-    .sous li:hover{
-        border-bottom: 1px solid rgb(1, 46, 0);
-    }
-
-    .sous a{
-        padding:10px;
-        border-bottom:none;
-    }
-
-    .sous a:hover{
-        border-bottom: none;
-        background-color: #fff;
-    }
-
-    
-    .Bien{
-        background-color: #dddddd;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 25vw;
-        padding: 5%;
-        border-radius: 3%;
-    }
-    .Bien:hover{
-        cursor: pointer;
-    }
-
-    .Bien p{
-        text-align: center;
-    }
-
-    .produits_container{
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 40px;
-        grid-auto-rows: minmax(10px, auto);
-    }
-
-    .produits_titre{
-        display: flex;
-        width: 100vw;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .block_filtre{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-        justify-content: center;
-        margin: 2vw;
-    }
-
-    @media (max-width: 1250px) { 
-        .Bien{
-        width: 40vw;
-    }
-    }
-
-    @media (max-width: 915px) { 
-        .Bien{
-        width: 90vw;
-    }
-    }
-    </style>
+</style>
--- clientVueJS/src/views/ProduitsView.vue
+++ clientVueJS/src/views/ProduitsView.vue
@@ -1,18 +1,106 @@
 <script setup>  
     import productItem from '../components/productVue.vue';
-    import Data from '../assets/json/achat.json'
+    import Data from '../assets/json/achat.json';
+    import axios from 'axios';
+</script>
 
-    
-    
+<script>
+    export default {
+        data() {
+            return {
+            categories: [],
+            categoriesParent:[],
+            categoriesEnfant:[],
+            produits: [],
+            selectedCategory: null
+            };
+        },
+        mounted() {
+            this.getAllCategories();
+            this.getAllProducts();
+        },
+        computed:{
+            CategoriesParent(){
+            let categoriesParent = [];
+            for(let categorie of this.categories){
+                if(categorie.categorieParentId==null){
+                categoriesParent.push(categorie);
+                }
+            }
+            return categoriesParent;
+            }
+        },
+        methods: {
+
+            getAllCategories() {
+            axios.get('https://localhost:7259/api/Categories/GetAll')
+            .then(response => {
+                console.log(response.data)
+                this.categories = response.data;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+            },
+
+            getAllProducts() {
+            axios.get('https://localhost:7259/api/Produits/GetAll')
+            .then(response => {
+                console.log(response.data)
+                this.produits = response.data;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+            },
+
+            getCategoriesEnfant(categorieParent){
+            let categoriesEnfant = [];
+            for(let categorie of this.categories){
+                if(categorie.categorieParentId == categorieParent.categorieId ){
+                categoriesEnfant.push(categorie);
+                }
+            }
+            return categoriesEnfant;
+            },
+
+            selectCategory(category) {
+            this.selectedCategory = category;
+            this.getAllProducts();
+            },
+
+            filterProducts() {
+            if (!this.selectedCategory) {
+                return this.produits;
+            }
+            return this.produits.filter((produit) => {
+                return produit.categorieId === this.selectedCategory.categorieId;
+            });
+            },
+            
+        },
+    };
 </script>
 
 <template>
-    <main>
-        <div >
-            <productItem/>
-        </div>
-        
-    </main>
+    <nav>
+    <ul>
+      <li class="deroulant" v-for="catParent in CategoriesParent" :key="catParent.categorieId">
+        <a>{{ catParent.libelle }}</a>
+
+        <ul class="sous">
+          <li v-for="catEnfant in getCategoriesEnfant(catParent)" :key="catEnfant.categorieId">
+            <a @click="selectCategory(catEnfant)">{{ catEnfant.libelle }}</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+  <div class="produits_container">
+
+    <productItem v-for="produit in filterProducts()" :key="produit.produitId" :libelle="produit.libelle"></productItem>
+
+  </div>
 </template>
   
 <style>
@@ -25,5 +113,123 @@ main{
     justify-content: center;
     align-items: center;
 }
+body{
+        height: 100vh;
+    }
+    img{
+        height: 10%;
+    }
+    nav{
+        width: 100%;
+        background-color: #fff;
+        top: 0px;
+    }
+
+    nav ul{
+        width: 92vw;
+        display: flex;
+        justify-content: space-around;
+        
+        
+    }
+
+    nav ul li{
+        display: flex;
+        width:fit-content;
+        text-align: center;
+        align-items: center;
+        position: relative;
+        margin: 12px;
+    }
+
+    nav ul::after{
+        content:"";
+        clear: both;
+    }
+
+    nav a{
+        text-decoration: none;
+        color: black;
+        border-bottom: 2px solid transparent;
+    }
+
+    nav a:hover{
+        color: var(--first-color);
+        border-bottom: 2px solid rgb(1, 46, 0);
+    }
+
+    .sous{
+        display: none;
+        box-shadow: 0px 1px 2px #CCC;
+        background-color: #fff;
+        position: absolute;
+        width: 200px;
+        height: fit-content;
+        top:100%;
+        z-index: 1000;
+    }
+
+    nav > ul li:hover .sous{
+        display: block;
+    }
+
+    .sous li{
+        float: none;
+        width: fit-content;
+        text-align:left;
+        cursor: pointer;
+    }
+
+    .sous li:hover{
+        border-bottom: 1px solid rgb(1, 46, 0);
+    }
+
+    .sous a{
+        padding:10px;
+        border-bottom:none;
+    }
+
+    .sous a:hover{
+        border-bottom: none;
+        background-color: #fff;
+    }
+
+    
+    
+
+    .produits_container{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 40px;
+        grid-auto-rows: minmax(10px, auto);
+    }
+
+    .produits_titre{
+        display: flex;
+        width: 100vw;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .block_filtre{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        justify-content: center;
+        margin: 2vw;
+    }
+
+    @media (max-width: 1250px) { 
+        .Bien{
+        width: 40vw;
+    }
+    }
+
+    @media (max-width: 915px) { 
+        .Bien{
+        width: 90vw;
+    }
+    }
     
 </style>
