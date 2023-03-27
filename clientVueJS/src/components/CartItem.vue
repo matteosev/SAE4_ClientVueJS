6ca7544462171e7b3030dc7f220c77a2f50b494d
jpoulain58 <jpoulain58@gmail.com>
2023-03-27 13:41:03 +0200

    panier

--- clientVueJS/src/components/CartItem.vue
+++ clientVueJS/src/components/CartItem.vue
@@ -47,31 +47,102 @@ export default {
             <button v-on:click="cartStore.deleteLine(cartStore.findLineId(variante.varianteId))">Supprimer</button>
         </div>
     </div>
+    
 </template>
 
 <style scoped>
 
-.cartItem{
-    margin: 10px;
+
+.cartItem {
     display: flex;
-    width: calc(100% - 20px);
+    align-items: center;
+    justify-content: space-between;
+    background-color: #f5f5f5;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    border-radius: 10px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
+    width: 100%;
+    max-width: 1000px;
+  }
+
+.cartItem img {
+  width: 400px;
+  height: auto;
+  object-fit: cover;
+  border-radius: 5px;
 }
 
-.cartItem > img{
-    width: 30%;
+.container-overview {
+  flex: 1;
+  padding: 0 1rem;
 }
 
-.cartItem > .container-overview{
-    width: 50%;
+.container-overview h1 {
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+  color: var(--first-color);
 }
 
-.cartItem > .container-buttons{
-    width: 20%;
-    display: flex;
-    flex-direction: column;
+.container-overview p {
+  font-size: 0.9rem;
+  margin: 0.2rem 0;
 }
 
-.cartItem > .container-buttons > button{
-    height: 50%;
+.container-buttons {
+  display: flex;
+  flex-direction: column;
+}
+
+.container-buttons button {
+  background-color: var(--first-color);
+  border: none;
+  color: white;
+  padding: 0.5rem 1rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 0.9rem;
+  font-weight: 500;
+  margin: 0.5rem 0;
+  cursor: pointer;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
+  transition: all 0.3s;
+}
+
+.container-buttons button:hover {
+  background-color: #5a5a5a;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12);
+}
+e
+
+.checkout {
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+
+.buy-now-btn {
+  align-self: center;
+  background-color: var(--first-color);
+  border: none;
+  color: white;
+  padding: 0.75rem 1.5rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 1rem;
+  font-weight: 500;
+  margin: 0.5rem;
+  cursor: pointer;
+  border-radius: 25px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
+  transition: all 0.3s;
+}
+
+.buy-now-btn:hover {
+  background-color: #5a5a5a;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12);
 }
 </style>
--- clientVueJS/src/views/Cart.vue
+++ clientVueJS/src/views/Cart.vue
@@ -31,12 +31,73 @@ export default {
 
 <template>
     <main>
+        <h1 class="cart-title">Votre Panier Civilité Nom</h1>
+    <div class="cart-wrapper">
+      <div class="cart-container">
         <CartItem v-for="line of cartStore.lines" :variante="line.variante" :quantity="line.quantity"></CartItem>
-    </main>
+      </div>
+      <div class="checkout">
+        <button @click="purchase" class="buy-now-btn">Acheter maintenant</button>
+      </div>
+    </div>
+  </main>
     <footer>
         footer
     </footer>
 </template>
 
 <style scoped>
+*{
+    font-family: 'Space-Grotesk-bold';
+}
+.cart-title {
+    text-align: center;
+    font-size: 2rem;
+    font-weight: bold;
+    color: var(--first-color);
+    margin: 1rem 0;
+  }
+.cart-wrapper {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .cart-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+    margin-bottom: 1rem;
+  }
+
+.checkout {
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+}
+
+.buy-now-btn {
+  background-color: var(--first-color);
+  border: none;
+  color: white;
+  padding: 0.75rem 1.5rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-weight: 500;
+  margin: 0.5rem;
+  cursor: pointer;
+  border-radius: 25px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s;
+}
+
+.buy-now-btn:hover {
+  background-color: grey;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
 </style>
