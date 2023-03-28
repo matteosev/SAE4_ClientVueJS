5e882ca8ad339b77e82db8f168769dc8b9980c69
Maxence <maxencedubois22@gmail.com>
2023-03-28 09:13:33 +0200

    premier jet d'une commande

--- clientVueJS/src/components/ProfilComponents/MesCommandesProfil.vue
+++ clientVueJS/src/components/ProfilComponents/MesCommandesProfil.vue
@@ -6,17 +6,18 @@
             Mes Commandes
         </h2>
     </div>
-        <div class="container-commande">
-            <h2 class="title-numero-commande"> Commande N° 42</h2>
-            <div class="article-commande">
-                    Articles : 
-            </div>
-            <div class="details-commande">
-                <p class="prix-commande">Prix : 1560.33 €</p>
-                <p class="status-commande"> Status commande : </p>
-            </div>
+    <div class="container-commande">
+        <h2 class="title-numero-commande"> Commande N° 42</h2>
+        <div class="article-commande">
+            Articles :
         </div>
-    
+        <div class="details-commande">
+            <p class="prix-commande">Prix : 1560.33 €</p>
+            <p class="status-commande"> Status commande : </p>
+        </div>
+        <button class="button-details"> <img class="icon-plus" src="../../assets/IconProfil/icons8-plus-32.png" alt="plus">
+            Voir le details</button>
+    </div>
 </template>
 
 <style scoped>
@@ -41,7 +42,7 @@
 
 }
 
-.container-commande{
+.container-commande {
     display: flex;
     position: relative;
     justify-content: center;
@@ -51,17 +52,70 @@
     border-radius: 10px;
     margin: 20px;
     height: 10%;
+    transition: 0.3s ease-in-out;
 }
 
-.title-numero-commande{
+.container-commande:hover {
+    box-shadow: 0 0 11px rgba(33, 33, 33, .2);
+}
+
+.title-numero-commande {
     position: absolute;
     top: 15%;
     left: 5%;
 }
 
-.details-commande{
+.details-commande {
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+
+.prix-commande {
+    position: absolute;
+    top: 70%;
+    left: 5%;
+}
+
+.status-commande {
+    position: absolute;
+    top: 20%;
+    left: 80%;
+}
+
+.article-commande {
+    position: absolute;
+    top: 20%;
+}
+
+.icon-plus {
+    width: 25px;
+    height: auto;
+    padding-right: 10px;
+}
+
+.button-details {
+    opacity: 0;
+    padding: 15px;
+    width: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    background: #6F9575;
+    color: white;
+    font-size: 14px;
+    text-transform: uppercase;
+    border-radius: 10px;
+    position: absolute;
+    top: 80%;
+    left: 75%;
+    transition: 0.3s ease-in-out;
+}
+
+.container-commande:hover .button-details {
+    opacity: 1;
 }
 </style>
--- clientVueJS/src/views/UnProduitView.vue
+++ clientVueJS/src/views/UnProduitView.vue
@@ -1,16 +1,13 @@
-<script setup>  
+<script>
 import axios from 'axios';
-import { Navigation} from 'swiper';
+import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { useAuthStore } from '../api/auth';
 import { useCartStore } from '../stores/cart';
-</script>
 
-<script>
 export default {
     props: ['id'],
-    data() 
-    {
+    data() {
         return {
             authStore: useAuthStore(),
             cartStore: useCartStore(),
@@ -20,40 +17,35 @@ export default {
             quantity: 1
         };
     },
-    created()
-    {
+    created() {
         axios.get('https://localhost:7259/api/Produits/GetById/' + this.id).then(response => this.produit = response.data).catch(error => console.error(error));
-        
+
         axios.get('https://localhost:7259/api/Variantes/GetAllVariantesByProduitIdAsync/' + this.id)
-        .then(response => 
-        {
-            //console.log(this.produit) // eslint-disable-line no-console
-            this.variantes = response.data
-            this.selectedVariante = this.variantes[0];
-            for (let variante of this.variantes)
-                axios.get('https://localhost:7259/api/Variantes/GetAllVariantePhotosAsync/' + variante.varianteId).then(response => variante.photos = response.data).catch(error => console.error(error));
-            //console.log(this.variantes) // eslint-disable-line no-console
-            //console.log(this.selectedVariante) // eslint-disable-line no-console
-        }
-        ).catch(error => console.error(error));
+            .then(response => {
+                //console.log(this.produit) // eslint-disable-line no-console
+                this.variantes = response.data
+                this.selectedVariante = this.variantes[0];
+                for (let variante of this.variantes)
+                    axios.get('https://localhost:7259/api/Variantes/GetAllVariantePhotosAsync/' + variante.varianteId).then(response => variante.photos = response.data).catch(error => console.error(error));
+                //console.log(this.variantes) // eslint-disable-line no-console
+                //console.log(this.selectedVariante) // eslint-disable-line no-console
+            }
+            ).catch(error => console.error(error));
     },
-    mounted()
-    {
+    mounted() {
     },
     computed:
     {
     },
-    methods: 
+    methods:
     {
-        colorClickHandler(event, newSelectedVariante)
-        {
+        colorClickHandler(event, newSelectedVariante) {
             this.selectedVariante = newSelectedVariante;
             for (let divColor of event.target.parentNode.children)
                 divColor.style.borderRadius = ""
             event.target.style.borderRadius = "100%"
         },
-        addToCart(event)
-        {
+        addToCart(event) {
             if (!this.authStore.isAuthenticated)
                 this.$router.push({ path: '/se-connecter', query: { redirectURL: encodeURIComponent('/produit/' + this.id) } });
             else
@@ -78,15 +70,20 @@ export default {
 
                 <div id="product-text">
                     <h2>Description</h2>
-                    <p style="text-align: justify;">{{  this.produit.description }}</p>
+                    <p style="text-align: justify;">{{ this.produit.description }}</p>
                     <h2>Entretien</h2>
-                    <p style="text-align: justify;">{{  this.produit.instructionsEntretien }}</p>
+                    <p style="text-align: justify;">{{ this.produit.instructionsEntretien }}</p>
                     <h2>Fiche Technique</h2>
-                    <p v-if="this.produit.matiere != null && this.produit.matiere != ''">Matière : {{  this.produit.matiere }}</p>
-                    <p v-if="this.produit.revetement != null && this.produit.revetement != ''">Revêtement : {{  this.produit.revetement }}</p>
-                    <p v-if="this.produit.poidsColis != null && this.produit.poidsColis != ''">Poids du colis : {{  this.produit.poidsColis }}</p>
-                    <p v-if="this.produit.dimensionsTotale != null && this.produit.dimensionsTotale != ''">Dimensions du produit : {{  this.produit.dimensionsTotale }}</p>
-                    <p v-if="this.produit.dimensionsColis != null && this.produit.dimensionsColis != ''">Dimensions du colis : {{  this.produit.dimensionsColis }}</p>
+                    <p v-if="this.produit.matiere != null && this.produit.matiere != ''">Matière : {{ this.produit.matiere
+                    }}</p>
+                    <p v-if="this.produit.revetement != null && this.produit.revetement != ''">Revêtement : {{
+                        this.produit.revetement }}</p>
+                    <p v-if="this.produit.poidsColis != null && this.produit.poidsColis != ''">Poids du colis : {{
+                        this.produit.poidsColis }}</p>
+                    <p v-if="this.produit.dimensionsTotale != null && this.produit.dimensionsTotale != ''">Dimensions du
+                        produit : {{ this.produit.dimensionsTotale }}</p>
+                    <p v-if="this.produit.dimensionsColis != null && this.produit.dimensionsColis != ''">Dimensions du colis
+                        : {{ this.produit.dimensionsColis }}</p>
                 </div>
 
             </div>
@@ -96,12 +93,15 @@ export default {
                 <div id="sidepane">
                     <h1>{{ this.produit.libelle }}</h1>
                     <p v-if="this.selectedVariante.stock >= 10">En stock</p>
-                    <p v-else-if="this.selectedVariante.stock < 10 && this.selectedVariante.stock > 0">Presque en rupture ! Stock restant : {{ this.selectedVariante.stock }}</p>
+                    <p v-else-if="this.selectedVariante.stock < 10 && this.selectedVariante.stock > 0">Presque en rupture !
+                        Stock restant : {{ this.selectedVariante.stock }}</p>
                     <p v-else style="color: red">En rupture</p>
                     <p>Prix : {{ selectedVariante.prix }}€</p>
 
                     <div id="container-colors" v-if="this.variantes.length > 1">
-                        <div v-for="variante of this.variantes" v-on:click="$event => colorClickHandler($event, variante)" :key="variante.varianteId" :style="{width:30 +'px', height:30 + 'px', backgroundColor:variante.couleurHexa}"></div>
+                        <div v-for="variante of this.variantes" v-on:click="$event => colorClickHandler($event, variante)"
+                            :key="variante.varianteId"
+                            :style="{ width: 30 + 'px', height: 30 + 'px', backgroundColor: variante.couleurHexa }"></div>
                     </div>
 
                     <!-- Call To Action -->
@@ -109,7 +109,7 @@ export default {
                         <input type="number" min="0" v-model="quantity">
                         <div id="container-quantity">
                             <button v-on:click="$event => this.quantity += 1">+</button>
-                            <button v-on:click="$event => (this.quantity - 1 > 0)? this.quantity -= 1 : 0">-</button>
+                            <button v-on:click="$event => (this.quantity - 1 > 0) ? this.quantity -= 1 : 0">-</button>
                         </div>
                         <div style="width: 5%;"></div>
                         <button v-on:click="$event => addToCart($event)">Ajouter au Panier</button>
@@ -136,18 +136,25 @@ export default {
 </template>
   
 <style scoped>
-
 * {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
 
-#product-col-1{ width: 70%; padding-left: 10px;}
+#product-col-1 {
+    width: 70%;
+    padding-left: 10px;
+}
 
-#product-col-2{ width: 30%; }
+#product-col-2 {
+    width: 30%;
+}
 
-.swiper { height: calc(100vh - 80px);  /* full height - navbar heigt*/ }
+.swiper {
+    height: calc(100vh - 80px);
+    /* full height - navbar heigt*/
+}
 
-.swiper img { 
+.swiper img {
     max-height: 100%;
     max-width: 80%;
     position: absolute;
@@ -157,28 +164,34 @@ export default {
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 }
 
-#sidepane{
+#sidepane {
     margin: 10px;
     padding: 10px;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-    height: calc(100vh - 80px - 40px);                          /* device height - navbar height - some spacing */
+    height: calc(100vh - 80px - 40px);
+    /* device height - navbar height - some spacing */
     position: sticky;
-    top: 90px;                                                  /* navbar height + margin */
+    top: 90px;
+    /* navbar height + margin */
 }
 
-#sidepane > * { margin-bottom: 10px; }
+#sidepane>* {
+    margin-bottom: 10px;
+}
 
-#sidepane > h1{ margin-top: 5px; }
+#sidepane>h1 {
+    margin-top: 5px;
+}
 
 #sidepane button {
     border-width: 1px;
-    border-color: rgba(0,0,0, 0.2);
+    border-color: rgba(0, 0, 0, 0.2);
     border-style: solid;
     background-color: aliceblue;
     transition: box-shadow 0.1s ease-in-out;
 }
 
-#sidepane button:active{
+#sidepane button:active {
     box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
 }
 
@@ -190,40 +203,45 @@ export default {
     gap: 10px;
 }
 
-#container-colors > div {
-    border: 1px solid rgba(0,0,0, 0.2);
+#container-colors>div {
+    border: 1px solid rgba(0, 0, 0, 0.2);
     cursor: pointer;
     transform: scale(1);
     border-radius: 0%;
     transition: transform 0.3s ease-in-out, border-radius 0.6s ease-in-out;
 }
 
-#container-colors > div:hover { transform: scale(1.1);}
+#container-colors>div:hover {
+    transform: scale(1.1);
+}
 
-#container-buy { width: 100%; display: flex;}
+#container-buy {
+    width: 100%;
+    display: flex;
+}
 
-#container-buy input[type=number]{
-    width: 20%; 
+#container-buy input[type=number] {
+    width: 20%;
     text-align: center;
-    border-color: rgba(0,0,0, 0.2);
+    border-color: rgba(0, 0, 0, 0.2);
     border-style: solid;
     border-width: 1px 0px 1px 1px;
 }
 
 #container-buy input[type=number]::-webkit-inner-spin-button,
-#container-buy input[type=number]::-webkit-outer-spin-button{
-    -webkit-appearance: none; 
+#container-buy input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
     margin: 0;
 }
 
-#container-quantity{
+#container-quantity {
     width: 10%;
-    border-color: rgba(0,0,0, 0.2);
+    border-color: rgba(0, 0, 0, 0.2);
     border-style: solid;
     border-width: 1px 1px 1px 0px;
 }
 
-#container-quantity > button {
+#container-quantity>button {
     text-align: center;
     padding: 0px;
     width: 100%;
@@ -231,11 +249,10 @@ export default {
     border-width: 0px 0px 0px 1px;
 }
 
-#container-quantity > button:first-child {
-    border-bottom: 1px solid rgba(0,0,0, 0.2);
+#container-quantity>button:first-child {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
 }
 
-#container-buy > button {
+#container-buy>button {
     width: 65%;
-}
-</style>
+}</style>
