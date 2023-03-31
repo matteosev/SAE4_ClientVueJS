485e0930957c369e444dca86311aa2e3909729e8
matteosev-IUT <severini.matt@gmail.com>
2023-03-31 08:21:23 +0200

    Affichage des avis OK mais manque un peu de CSS

---
+++ clientVueJS/src/components/CardAvis.vue
@@ -0,0 +1,54 @@
+<script setup>
+import axios from 'axios';
+</script>
+
+<script>
+export default {
+    props: ['avis'],
+    data()
+    {
+        return {
+        };
+    },
+    created()
+    {
+    },
+    methods:
+    {
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div id="avisHeader">
+            <h3>{{ avis.titre }}</h3>
+            <div>
+                <span v-for="i of [1,2,3,4,5]" class="dot" :class="[i <= avis.note ? 'dotChecked' : '']"></span>
+            </div>
+        </div> 
+        <p>{{ avis.texte }}</p>
+        <div id="avisFooter">
+            <p>{{ new Date(avis.date).toLocaleDateString('fr-FR') }}</p>
+            <p>{{ avis.clientId }}</p>
+        </div> 
+    </div>
+</template>
+
+<style>
+
+#avisHeader { display: flex; }
+#avisHeader > h3 { flex-grow: 1; }
+#avisFooter { display: flex; }
+#avisFooter > p:first-child { flex-grow: 1; }
+
+.dot{
+    height: 1em;
+    width: 1em;
+    background-color: rgba(0,0,0, 0.2);
+    border-radius: 50%;
+    display: inline-block;
+}
+
+.dotChecked { background-color: var(--first-color); }
+</style>
